5ad4bd890ea832c0cf68404b98a3658bf26b173b
Thiago Emidio <thiagofarbo@gmail.com>
2020-02-01 12:26:54 -0300

    Fix problema ao enviar email via formulário

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const nodemailer = require('nodemailer');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.render('index'))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 
-app.get('/send',(req, res) =>{
+app.get('/', (req, res) => res.render('index.ejs'))
+app.post('/send',(req, res) =>{
+
+const to = req.body.to;
+const subject = req.body.subject;
+const body = req.body.body;
 
 const user = 'youremail';
 const pass = 'yourpassword';
@@ -18,10 +27,11 @@ const transporter = nodemailer.createTransport({
 
 transporter.sendMail({
     from: user,
-    to: user,
+    to: to,
     replyTo: "youremailtoreplay",
-    subject: 'Test',
-    text: 'Testing',
+    subject: subject,
+    text: body,
+    html: '<h1>Enjoy :)</h1>'
 }).then(info =>{
     console.log(info)
     res.send(info);
